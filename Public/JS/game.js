00730e7f99387410f7717df4acb2a4ca956d52d8
Divyanshu Bisht <divyanshubisht5734@gmail.com>
2025-07-31 01:06:23 +0530

    second commit

--- Public/JS/game.js
+++ Public/JS/game.js
@@ -1,1 +1,20 @@
+// client1 dosen't directly connected to the person or client2 . client1 directly connected to the server first then to client2. And socket.io comes in work 
+// there are mainly three ways in which socket.io connect to the frontend.
+
+const { Chess } = require("chess.js");
+
+// 1) socket.io sends data to every client like ---------- group chats.
+// 2) socket.io sends data to a single chats like ---------one to one chats.
+// 3) socket.io sends data to a every client except oneself like ---------- shown a rendering message typing in whatsapp while chatting  it is also called broadcasting.
+
 const socket = io();
+
+// socket.emit("divyanshu")                          // emit means to send the data to server(backend).
+
+// socket.on("divyanshu neha" , function(){         // on means to receive a data from backend.
+//        console.log("accepted");
+       
+// })
+
+const chess = new Chess();
+const boardElemnt = document.querySelector(".chessBoard")
