d483ff310d2ec5b7f1257fc0b95b80a91643d66c
Divyanshu Bisht <divyanshubisht5734@gmail.com>
2025-07-31 01:01:39 +0530

    first commit

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const socket = require('socket.io')
 const { Chess } = require('chess.js')
 const http = require('http')
 const path = require('path')
-const { title } = require('process')
-const { log } = require('console')
 
 const app = express();
 const server = http.createServer(app)
@@ -12,8 +10,8 @@ const server = http.createServer(app)
 const io = socket(server)
 
 const chess = new Chess();
-let player1 = {} ;
-let player2 = "W" ;
+let players = {} ;
+let player1 = "W" ;
 
 app.set('view engine' , 'ejs')
 app.use(express.urlencoded({extended : true }))
@@ -27,7 +25,65 @@ app.get("/" , (req , res)=>{
 io.on("connection" , function(uniquesocket){
        
     console.log("connected");
+
+//     uniquesocket.on( "divyanshu", function(){         // receive the data on backend from fontend.
+//          io.emit("divyanshu neha")                   // send again the data to frontend.
+         
+//     })
+ 
+      // uniquesocket.on("disconnect" , ()=>{
+      //         console.log("disconnected");
+              
+      // })
+
+      if(!players.white){
+              
+            players.white = uniquesocket.id ;
+            uniquesocket.emit("playerRole" , "W");
+      }else if (!players.black){
+            players.black = uniquesocket.id ;
+            uniquesocket.emit("playerRole" , "B");
+      }else{
+             
+            uniquesocket.emit("spectator")
+      }
     
+
+      uniquesocket.on('disconnect' , function(){
+             
+             if(uniquesocket.id === players.white){
+                  delete players.white;
+             }else if(uniquesocket.id === players.black){
+                    delete players.black;
+             }
+      })
+
+      uniquesocket.on("move" , (move)=>{
+
+            try{
+
+                  if( chess.turn() === "W" && uniquesocket.id != players.white )  return ;
+                  if( chess.turn() === "B" && uniquesocket.id != players.black )  return ;
+
+                  const result = chess.move(move) ;
+
+                  if(result){
+                        player1 = chess.turn();
+                        io.emit();
+                        io.emit("boardState" , chess.fen())
+                  }else{
+                         console.log("Invalid move" , move);
+                         uniquesocket.emit("Invalid Move" , move)
+                         
+                  }
+
+
+            }catch(err){
+                     console.log(err);
+                     
+            }
+
+      })
 })
 
 server.listen(3000 , function(){
